0e14c5727b796bfb81c194cffb7f5cdecdf2cd6a
zhang1003533565 <zeshengzhang@gmail.con>
2025-04-12 14:56:22 +0800

    优化

--- myproject/tech_application/models.py
+++ myproject/tech_application/models.py
@@ -5,19 +5,19 @@ class TechApplication(models.Model):
     create_time = models.CharField(max_length=100, verbose_name="写入时间")
     dmsyl = models.CharField(max_length=100, verbose_name="地膜使用量")
     nysyl = models.CharField(max_length=100, verbose_name="农药使用量")
-    sdjxhcd = models.CharField(max_length=100, verbose_name="机械化程度")
+    jxhcd = models.CharField(max_length=100, verbose_name="机械化程度")  # 总体机械化
+    sdjxhcd = models.CharField(max_length=100, verbose_name="水稻机械化程度")
+    sdjscd = models.CharField(max_length=100, verbose_name="水稻收割机械化程度")
     id = models.CharField(max_length=100, primary_key=True, verbose_name="自增主键")
     dsjzx_taskid = models.CharField(max_length=100, verbose_name="批次号")
-    jxhcd = models.CharField(max_length=100, verbose_name="机械化程度")
     jgmj = models.CharField(max_length=100, verbose_name="机械面积")
     sdjmj = models.CharField(max_length=100, verbose_name="水稻机械面积")
+    jdjsmj = models.CharField(max_length=100, verbose_name="水稻机捕面积")
     jhpt_delete = models.CharField(max_length=100, verbose_name="逻辑删除")
-    sdjscd = models.CharField(max_length=100, verbose_name="机械化程度")
     jhpt_update_time = models.CharField(max_length=100, verbose_name="修改时间")
     hfsul = models.CharField(max_length=100, verbose_name="化肥施用量")
     jz = models.CharField(max_length=100, verbose_name="镇")
     dmfgmj = models.CharField(max_length=100, verbose_name="地膜覆盖面积")
-    jdjsmj = models.CharField(max_length=100, verbose_name="水稻机械收获面积")
 
     def __str__(self):
         return f"{self.jz} - {self.dmsyl}"
---
+++ sql语句/农业技术应用（tech_application）.sql
@@ -0,0 +1,22 @@
+-- 青浦区农业技术应用
+INSERT INTO tech_application (create_time, jxhcd, jhpt_delete, jz, dmsyl, sdjxhcd, dsjzx_taskid, jdjsmj, sdjscd, jhpt_update_time, jgmj, hfsul, sdjmj, nysyl, id, dmfgmj) VALUES ('2025-03-24 08:10:59.0', '100', '0', '赵巷镇', NULL, '84.07960199', '20250410', '3718', '100', '2025-03-24 08:10:59.0', '4422', '283.5701243', '4422', '5.879396985', 1, NULL);
+-- 青浦区农业技术应用
+INSERT INTO tech_application (create_time, jxhcd, jhpt_delete, jz, dmsyl, sdjxhcd, dsjzx_taskid, jdjsmj, sdjscd, jhpt_update_time, jgmj, hfsul, sdjmj, nysyl, id, dmfgmj) VALUES ('2025-03-24 08:10:59.0', '100', '0', '徐泾镇', '2.13', '91.32402824', '20250410', '796.93', '100', '2025-03-24 08:10:59.0', '872.64', '221.6209587', '872.64', '2.939698492', 2, '246.5032709');
+-- 青浦区农业技术应用
+INSERT INTO tech_application (create_time, jxhcd, jhpt_delete, jz, dmsyl, sdjxhcd, dsjzx_taskid, jdjsmj, sdjscd, jhpt_update_time, jgmj, hfsul, sdjmj, nysyl, id, dmfgmj) VALUES ('2025-03-24 08:10:59.0', '100', '0', '华新镇', '3.6', '79.93082008', '20250410', '5250.16', '100', '2025-03-24 08:10:59.0', '6568.38', '603.7862338', '6568.38', '8.819095477', 3, '593.0514606');
+-- 青浦区农业技术应用
+INSERT INTO tech_application (create_time, jxhcd, jhpt_delete, jz, dmsyl, sdjxhcd, dsjzx_taskid, jdjsmj, sdjscd, jhpt_update_time, jgmj, hfsul, sdjmj, nysyl, id, dmfgmj) VALUES ('2025-03-24 08:10:59.0', '100', '0', '重固镇', '16.9', '99.34054938', '20250410', '10761.82', '100', '2025-03-24 08:10:59.0', '10833.26', '852.4554197', '10833.26', '13.71859296', 4, '1785.800437');
+-- 青浦区农业技术应用
+INSERT INTO tech_application (create_time, jxhcd, jhpt_delete, jz, dmsyl, sdjxhcd, dsjzx_taskid, jdjsmj, sdjscd, jhpt_update_time, jgmj, hfsul, sdjmj, nysyl, id, dmfgmj) VALUES ('2025-03-24 08:10:59.0', '100', '0', '白鹤镇', '95.9', '93.75775042', '20250410', '11363.73', '100', '2025-03-24 08:10:59.0', '12120.31', '2841.808907', '12120.31', '29', 5, '9873.749803');
+-- 青浦区农业技术应用
+INSERT INTO tech_application (create_time, jxhcd, jhpt_delete, jz, dmsyl, sdjxhcd, dsjzx_taskid, jdjsmj, sdjscd, jhpt_update_time, jgmj, hfsul, sdjmj, nysyl, id, dmfgmj) VALUES ('2025-03-24 08:10:59.0', '100', '0', '朱家角镇', '2.6', '87.1187379', '20250410', '19229.78', '100', '2025-03-24 08:10:59.0', '22073.07', '1758.13477', '22073.07', '28', 6, '208.3701683');
+-- 青浦区农业技术应用
+INSERT INTO tech_application (create_time, jxhcd, jhpt_delete, jz, dmsyl, sdjxhcd, dsjzx_taskid, jdjsmj, sdjscd, jhpt_update_time, jgmj, hfsul, sdjmj, nysyl, id, dmfgmj) VALUES ('2025-03-24 08:10:59.0', '100', '0', '练塘镇', '1.4', '46.5214631', '20250410', '9526.2', '100', '2025-03-24 08:10:59.0', '20477', '3575.601136', '20477', '46.05527638', 7, '83.075688');
+-- 青浦区农业技术应用
+INSERT INTO tech_application (create_time, jxhcd, jhpt_delete, jz, dmsyl, sdjxhcd, dsjzx_taskid, jdjsmj, sdjscd, jhpt_update_time, jgmj, hfsul, sdjmj, nysyl, id, dmfgmj) VALUES ('2025-03-24 08:10:59.0', '100', '0', '金泽镇', '1.3', '76.7617933', '20250410', '21184.95', '100', '2025-03-24 08:10:59.0', '27598.3', '1827.064124', '27598.3', '31', 8, '115.7612046');
+-- 青浦区农业技术应用
+INSERT INTO tech_application (create_time, jxhcd, jhpt_delete, jz, dmsyl, sdjxhcd, dsjzx_taskid, jdjsmj, sdjscd, jhpt_update_time, jgmj, hfsul, sdjmj, nysyl, id, dmfgmj) VALUES ('2025-03-24 08:10:59.0', '100', '0', '夏阳街道', '19.2', '95.8386454', '20250410', '5314.78', '100', '2025-03-24 08:10:59.0', '5545.55', '442.3693939', '5545.55', '7.83919598', 9, '479.3875766');
+-- 青浦区农业技术应用
+INSERT INTO tech_application (create_time, jxhcd, jhpt_delete, jz, dmsyl, sdjxhcd, dsjzx_taskid, jdjsmj, sdjscd, jhpt_update_time, jgmj, hfsul, sdjmj, nysyl, id, dmfgmj) VALUES ('2025-03-24 08:10:59.0', '100', '0', '盈浦街道', '1.1', '100', '20250410', '1911.08', '100', '2025-03-24 08:10:59.0', '1911.08', '134.3686127', '1911.08', '1.959798995', 10, '88.52327409');
+-- 青浦区农业技术应用
+INSERT INTO tech_application (create_time, jxhcd, jhpt_delete, jz, dmsyl, sdjxhcd, dsjzx_taskid, jdjsmj, sdjscd, jhpt_update_time, jgmj, hfsul, sdjmj, nysyl, id, dmfgmj) VALUES ('2025-03-24 08:10:59.0', '100', '0', '香花桥街道', '3.7', '94.03790108', '20250410', '11606.28', '100', '2025-03-24 08:10:59.0', '12342.13', '885.6113112', '12342.13', '14.69849246', 11, '724.0250493');
